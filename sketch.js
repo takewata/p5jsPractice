2f3a79bc56a729c172f0ac09de3509bb0f1f1afe
takewata <wata.tsth998@gmail.com>
2021-08-04 01:51:35 +0900

    tried tutorial

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+    createCanvas(400,400)
+}
+
+function draw(){
+    if(mouseIsPressed){
+        fill(0)
+    }
+    else{
+        fill(255)
+    }
+    ellipse(mouseX,mouseY,80,80)
+}
+
+function keyPressed(){
+    save("scketch.png"); //pngもしくはjpgで保存可能
+}  
